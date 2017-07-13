1d29fe7321e6bc687eb1a8e7d3be1626f29df586
gabytrejo <gabytrejo303@gmail.com>
2017-07-13 15:56:00 +0000

    Planet Cards

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var path = require('path');
 var app = express();
 
 app.use('/data',express.static(path.join(__dirname,'data')));
+app.use('/materialize',express.static(path.join(__dirname,'node_modules/materialize-css')));
+app.use('/planets.js',express.static(path.join(__dirname,'/planets.js')));
 
 app.get('/', function(require, response){
 	response.sendFile(__dirname+'/index.html');
